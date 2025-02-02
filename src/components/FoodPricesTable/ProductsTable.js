3a8406472fac4dc592790a6386383711ab778b32
Juris Daksa <juris.daksa@outlook.com>
2025-02-02 12:17:42 +0200

    Fixed warning for spread-operatoring key prop of a table row

--- src/components/FoodPricesTable/ProductsTable.js
+++ src/components/FoodPricesTable/ProductsTable.js
@@ -4,33 +4,43 @@ const ProductsTable = ({ getTableProps, getTableBodyProps, headerGroups, prepare
   return (
     <table className="table table-hover" {...getTableProps()}>
       <thead className="thead-dark">
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())} className={`col-${column.width}`}>
-                {column.render('Header')}
-                <span>
-                  {column.isSorted
-                    ? column.isSortedDesc
-                      ? <i className="bi bi-arrow-down px-2"></i>
-                      : <i className="bi bi-arrow-up px-2"></i>
-                    : ''}
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
+        {headerGroups.map(headerGroup => {
+          const { key, ...restHeaderGroupProps } = headerGroup.getHeaderGroupProps();
+          return (
+            <tr key={key} {...restHeaderGroupProps}>
+              {headerGroup.headers.map(column => {
+                const { key, ...restColumnProps } = column.getHeaderProps(column.getSortByToggleProps());
+                return (
+                  <th key={key} {...restColumnProps} className={`col-${column.width}`}>
+                    {column.render('Header')}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? <i className="bi bi-arrow-down px-2"></i>
+                          : <i className="bi bi-arrow-up px-2"></i>
+                        : ''}
+                    </span>
+                  </th>
+                );
+              })}
+            </tr>
+          );
+        })}
       </thead>
       <tbody className="table-group-divider" {...getTableBodyProps()}>
-        {page.map((row, i) => {
+        {page.map(row => {
           prepareRow(row);
+          const { key, ...restRowProps } = row.getRowProps();
           return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()} className={`col-${cell.column.width}`}>
-                  {cell.render('Cell')}
-                </td>
-              ))}
+            <tr key={key} {...restRowProps}>
+              {row.cells.map(cell => {
+                const { key, ...restCellProps } = cell.getCellProps();
+                return (
+                  <td key={key} {...restCellProps} className={`col-${cell.column.width}`}>
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
             </tr>
           );
         })}
